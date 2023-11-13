2902ac9f5e39926741037cea78e2d8d9815e3a78
sadicberat <sadicberat@gmail.com>
2023-11-13 15:52:42 +0300

    13.11.23

--- bitirme_prjem2/app/build.gradle.kts
+++ bitirme_prjem2/app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    //firebase için aşağıda 1 satır
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -37,10 +40,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//aşağıdakiler firebase için (iki satır)
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }

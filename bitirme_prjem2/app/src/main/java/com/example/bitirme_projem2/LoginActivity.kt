0999fdbb8fbb7b073ba1a951e98b8cc0646600f4
sadicberat <sadicberat@gmail.com>
2023-11-21 04:58:48 +0300

    21.11.23 v3 firebase Authentication (mail & password) successful

--- bitirme_prjem2/app/src/main/java/com/example/bitirme_projem2/LoginActivity.kt
+++ bitirme_prjem2/app/src/main/java/com/example/bitirme_projem2/LoginActivity.kt
@@ -1,79 +1,68 @@
 package com.example.bitirme_projem2
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.firebase.ui.auth.AuthUI
-import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
 
-
-
+    private lateinit var register: Button
+    private lateinit var login: Button
+    private lateinit var username: TextView
+    private lateinit var password: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_login)
 
-        val signUp: TextView = findViewById(R.id.signUp)
-        signUp.setOnClickListener {
+        auth = FirebaseAuth.getInstance()
+
+        register = findViewById(R.id.Button_register)
+        login = findViewById(R.id.login_button)
+        username = findViewById(R.id.user_name)
+        password = findViewById(R.id.user_password)
+
+        register.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
-        val loginButton: TextView = findViewById(R.id.login_button)
-        loginButton.setOnClickListener {
-            onLoginButtonClick(it) // Butona tıklanınca onLoginButtonClick fonksiyonunu çağır
+        login.setOnClickListener {
+            onLoginButtonClick(it)
         }
     }
 
     private fun onLoginButtonClick(view: View) {
-        // EditText'leri al
-        val usernameEditText: TextView = findViewById(R.id.user_name)
-        val passwordEditText: TextView = findViewById(R.id.user_password)
+        val email = username.text.toString()
+        val password = password.text.toString()
 
-        // Girdileri al
-        val enteredUsername = usernameEditText.text.toString()
-        val enteredPassword = passwordEditText.text.toString()
-
-        // Kontrol et ve mesaj göster
-        if (checkCredentials(enteredUsername, enteredPassword)) {
-            Toast.makeText(this, "Giriş Başarılı", Toast.LENGTH_SHORT).show()
-            startMainActivity()
-        } else {
-            Toast.makeText(this, "Giriş Başarısız. Kullanıcı adı veya şifre yanlış.", Toast.LENGTH_SHORT).show()
-        }
+        signIn(email, password)
     }
 
-    private fun checkCredentials(username: String, password: String): Boolean {
-        // Kullanıcı adı ve şifreyi kontrol et
-        val correctUsername = "admin"
-        val correctPassword = "admin"
-
-        return username == correctUsername && password == correctPassword
-    }
-    private fun startMainActivity(){
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-    }
     private fun signIn(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Giriş başarılıysa yapılacak işlemler
                     Toast.makeText(this, "Giriş Başarılı", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
+                    startMainActivity()
                 } else {
                     // Giriş başarısızsa kullanıcıya uyarı ver
                     Toast.makeText(this, "Giriş Başarısız. Kullanıcı adı veya şifre yanlış.", Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- bitirme_prjem2/app/src/main/java/com/example/bitirme_projem2/SignUpActivity.kt
+++ bitirme_prjem2/app/src/main/java/com/example/bitirme_projem2/SignUpActivity.kt
@@ -1,24 +1,76 @@
 package com.example.bitirme_projem2
 
-
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.bitirme_projem2.LoginActivity
+import com.example.bitirme_projem2.MainActivity
+import com.example.bitirme_projem2.R
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var userName: TextView
+    private lateinit var userPassword: TextView
+    private lateinit var register: Button
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_signup)
 
-        val button_geri_signUp: Button = findViewById(R.id.button_geri_signUp)
-        button_geri_signUp.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+        userName = findViewById(R.id.signUp_name)
+        userPassword = findViewById(R.id.signUp_password)
+        register = findViewById(R.id.button_signUp)
+
+        auth = FirebaseAuth.getInstance()
+
+        val name = userName.text.toString()
+        val password = userPassword.text.toString()
+
+        register.setOnClickListener {
+            onRegisterButtonClick(it)
+
+
         }
 
 
+        val buttonGeriSignUp: Button = findViewById(R.id.button_geri_signUp)
+        buttonGeriSignUp.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun onRegisterButtonClick(view: View) {
+        val email = userName.text.toString()
+        val password = userPassword.text.toString()
+
+        signUn(email, password)
+    }
+    private fun signUn(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Giriş başarılıysa yapılacak işlemler
+                    Toast.makeText(this, "kayıt oluşturuldu giriş yapılıyor", Toast.LENGTH_SHORT).show()
+                    startMainActivity()
+                } else {
+                    // Giriş başarısızsa kullanıcıya uyarı ver
+                    Toast.makeText(this, "Giriş Başarısız. Kullanıcı adı veya şifre kısımlarını doğru doldurun", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
-
+    companion object {
+        private const val TAG = "SignUpActivity"
+    }
 }
